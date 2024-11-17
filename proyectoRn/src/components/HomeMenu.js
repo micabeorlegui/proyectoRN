07ff7a3399a87ec300c137b1fe034547af4c4a74
jazbang <jbang@udesa.edu.ar>
2024-11-17 19:16:38 -0300

    Register casi completo, falta lo de los campos obligatorios

--- proyectoRn/src/components/HomeMenu.js
+++ proyectoRn/src/components/HomeMenu.js
@@ -3,6 +3,9 @@ import Home from '../screens/Home';
 import NewPost from '../screens/NewPost';
 import Profile from '../screens/Profile';
 import Users from '../screens/Users';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 const Tab = createBottomTabNavigator();
 
 const HomeMenu= (props) => {
@@ -10,24 +13,24 @@ const HomeMenu= (props) => {
         <Tab.Navigator >
         
           <Tab.Screen 
-            options={ { headerShown: false }}
+            options={{tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />, headerShown:false}}
             name="Home" 
             component={Home}
           />
 
           <Tab.Screen  
-            options={ { headerShown: false }}
+            options={{tabBarIcon: () => <MaterialIcons name="post-add" size={24} color="black" />, headerShown:false}}
             name="NewPost" 
             component={NewPost} 
           />
 
           <Tab.Screen  
-            options={ { headerShown: false }}
+            options={{tabBarIcon: () => <MaterialIcons name="account-circle" size={24} color="black" />, headerShown: false }}
             name="Profile" 
             component={Profile} 
           />
           <Tab.Screen
-            options={ { headerShown: false }}
+            options={{tabBarIcon: () => <FontAwesome name="users" size={24} color="black" />, headerShown:false}}
             name="Usuarios" 
             component={Users} 
           />
--- proyectoRn/src/screens/Register.js
+++ proyectoRn/src/screens/Register.js
@@ -1,21 +1,136 @@
-import { Component } from "react";
-import { View } from "react-native-web";
+import React, { Component } from "react";
+import { Text, View, TouchableOpacity, StyleSheet, TextInput} from 'react-native';
+import {db, auth} from '../firebase/config';
 
 
 class Register extends Component{
    constructor(){
        super();
        this.state={
-
-
+            email:'',
+            username:'',
+            password:'',
+            registered: false,
+            error:''
        }
    }
+
+   componentDidMount(){
+    auth.onAuthStateChanged(user=>{
+        if (user) {
+            this.props.navigation.navigate('HomeMenu') 
+        }
+    })
+}
+
+onSubmit(){
+
+    auth.createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(response=>{
+            if (this.state.email==='' || this.state.password==='' || this.state.username==='' ) {
+                this.setState({error: 'Todos los campos deben ser completados.'})
+                console.log(this.state.error)
+                return
+            }
+            
+            if (!this.state.email.includes('@')) {
+                this.setState({error: 'Email mal formateado'})
+                console.log(this.state.error)
+                return
+            }
+        
+            if (this.state.password.length<6) {
+                this.setState({error: 'La password debe tener una longitud mínima de 6 caracteres'})
+                console.log(this.state.error)
+                return
+            }
+
+            this.setState({registered: true})
+
+            db.collection('users').add({
+                owner: auth.currentUser.email,
+                email: this.state.email,
+                userName:this.state.username,
+                createdAt: Date.now()
+            })
+        })
+        .then(()=> this.props.navigation.navigate('Login'))
+        .catch(error=>{
+            this.setState({error:'Fallo en el regitro.'})
+        })
+}
+
    render () {
        return(
-           <View> </View>
+        <View style={styles.container}>
+            <Text style={styles.register}>Registro</Text>
+
+            <TextInput style={styles.field} 
+                keyboardType='email-address'
+                placeholder='Ingrese su email'
+                onChangeText={ text => this.setState({email:text}) }
+                value={this.state.email} />
+
+            <TextInput style={styles.field} 
+                keyboardType='default'
+                placeholder='Ingrese su nombre de usuario'
+                onChangeText={ text => this.setState({username:text}) }
+                value={this.state.username} />
+
+            <TextInput style={styles.field} 
+                keyboardType='default'
+                placeholder='Ingrese su contraseña'
+                secureTextEntry={true} 
+                onChangeText={ text => this.setState({password:text}) }
+                value={this.state.password}/> 
+
+            <TouchableOpacity style={styles.botonRegistro} onPress={() => this.onSubmit()}>
+                <Text style={styles.textoCentro}> Registrar </Text> 
+            </TouchableOpacity> 
+
+            <TouchableOpacity style={styles.ingresar} onPress={ ()=> this.props.navigation.navigate('Login')}>
+                <Text style={styles.textoCentro}>Ya tengo cuenta</Text>
+            </TouchableOpacity>
+        </View>
        )
    }
 
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width:'100%',
+      flex: 1,
+      padding: 20
+    },
+    register:{
+        fontWeight:'bold',
+        lineHeight:40,
+        fontSize:20
+    },
+    ingresar:{
+        backgroundColor:'rgb(255,165,0)',
+        padding:8,
+        marginBottom:10,
+        marginTop:10,
+        borderRadius:10
+    },
+    botonRegistro:{
+        backgroundColor:'rgb(155,155,155)',
+        width:'25%',
+        borderRadius:10,
+        paddin:5
+    },
+    textoCentro:{
+        textAlign:'center'
+    },
+    field:{
+        borderWidth:1,
+        borderColor:'rgb(155,155,155)',
+        marginBottom:10,
+        paddingLeft:10
+    }
+  });
+
 export default Register
