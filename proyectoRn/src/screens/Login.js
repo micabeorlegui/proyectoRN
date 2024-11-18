5d0dc5b5cdd5ef7859ad19bac4c21f02c3be4a18
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-11-18 11:13:34 -0300

    modifique el Login

--- proyectoRn/src/screens/Login.js
+++ proyectoRn/src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity, StyleSheet, TextInput} from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, TextInput, Image} from 'react-native';
 import {db, auth} from '../firebase/config';
 
 
@@ -10,7 +10,7 @@ class Login extends Component{
         email: "",
         password: "",
         logued: false,
-        error: "",
+        errores: [],
        }
    }
 
@@ -23,42 +23,57 @@ class Login extends Component{
   }
 
    handleSubmit() { 
+    let errores= []
+    
+    if (this.state.email==='' || this.state.password==='' ) {
+        errores.push('Todos los campos deben ser completados.')
+    }
+
+
+    this.setState({ errores });
+
     auth
       .signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then((response) => this.setState({ logued: true }))
+      .then((response) => this.setState({ logued: true,  errores: [] }))
       .then( ()=>  this.props.navigation.navigate("HomeMenu"))
       .catch((error) => this.setState({ error: "Fallo el login" }));    
   }
    render () {
        return(
         <View style={styles.container}>
-            <Text style={styles.heading}>Ingresar</Text>
-            <TextInput
-                keyboardType="email-address"
-                placeholder="Ingrese su email"
-                onChangeText={(text) => this.setState({ email: text })}
-                value={this.state.email}
-            />
-            <TextInput
-                placeholder="Ingrese su contrasena"
-                secureTextEntry={true}
-                onChangeText={(text) => this.setState({ password: text })}
-                value={this.state.password}
-            />
-            <TouchableOpacity onPress={() => this.handleSubmit() }  style={[styles.button, styles.buttonSecondary]}>
-            <Text>Acceder</Text>
-            </TouchableOpacity>
-            <Text>Navegación cruzada a Register: </Text>
-            <TouchableOpacity
-                onPress={() => this.props.navigation.navigate("Register")}
-                style={styles.button}
-            >
-            <Text>No tengo cuenta</Text>
-            </TouchableOpacity>
-            <Text>
-            Navegación cruzada a ingresar a la app. Este paso se hará
-            automaticamente cuando veamos la funcionalidad de loguin{" "}
-            </Text>      
+            <Image source={require('../../assets/img/background.jpeg')} style={styles.backgroundImage}/>
+            <View style={styles.content}>
+                <Text style={styles.login}>Ingresar</Text>
+                <TextInput
+                    style={styles.field} 
+                    keyboardType="email-address"
+                    placeholder="Ingrese su email"
+                    onChangeText={(text) => this.setState({ email: text })}
+                    value={this.state.email}
+                />
+                <TextInput
+                    style={styles.field} 
+                    placeholder="Ingrese su contrasena"
+                    secureTextEntry={true}
+                    onChangeText={(text) => this.setState({ password: text })}
+                    value={this.state.password}
+                />
+                <TouchableOpacity onPress={() => this.handleSubmit() }  style={styles.botonLogin}>
+                <Text style={styles.textoCentro}>Acceder</Text>
+                {this.state.errores.length > 0 && (
+                        <View style={styles.errorContainer}>{this.state.errores.map((error, index) => (
+                            <Text key={index} style={styles.error}>{error}</Text>))}
+                        </View>
+                )}
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate("Register")}
+                    style={styles.noTengoCuenta}
+                >
+                <Text style={styles.textoCentro}>No tengo cuenta</Text>
+                </TouchableOpacity>
+                
+            </View>    
         </View>
        )
    }
@@ -68,23 +83,74 @@ class Login extends Component{
 
 const styles = StyleSheet.create({
     container: {
-      margin: 20,
+      width:'100%',
+      flex: 1,
+      padding: 20
     },
-    heading: {
-      fontSize: 30,
-      fontWeight: 700,
-      marginBottom: 10,
+    backgroundImage:{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
     },
-    button: {
-      backgroundColor: "#51b9e9",
-      borderRadius: 5,
-      padding: 10,
-      width: "100%",
-      alignItems: "center",
-      marginTop: 10,
+    content:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20
     },
-    buttonSecondary: {
-      backgroundColor: "#ffa500",
+    login:{
+        fontWeight:'bold',
+        lineHeight:40,
+        fontSize:24,
+        marginBottom: 25,
+        color:'#481E14'
     },
+    botonLogin:{
+        backgroundColor:'#A67B5B',
+        width:'50%',
+        borderRadius:10,
+        padding:4,
+        marginTop:20
+    },
+    noTengoCuenta:{
+        backgroundColor:'#B99470',
+        width:'60%',
+        borderRadius:10,
+        padding:4,
+        marginTop:20
+    },
+    textoCentro:{
+        textAlign:'center'
+    },
+    field:{
+        width: '100%',
+        height: 35,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        marginBottom: 15,
+        paddingLeft: 15,
+        backgroundColor: '#FBFBFB',
+        fontSize: 14,
+        shadowColor:"#000",
+        shadowOffset:{
+            width:0,
+            height:2
+        },
+        shadowOpacity:0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    error: {
+        color: "red",
+        marginBottom: 10,
+        textAlign: "center",
+    }
   });
+
 export default Login
