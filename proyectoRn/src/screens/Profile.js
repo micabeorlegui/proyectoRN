f92d0ea4057b482f3a827f96e0fdbddc695b8e74
jazbang <jbang@udesa.edu.ar>
2024-11-18 17:22:07 -0300

    manteniendo la estetica de la app

--- proyectoRn/src/screens/Profile.js
+++ proyectoRn/src/screens/Profile.js
@@ -23,11 +23,15 @@ class Profile extends Component{
 
    render () {
        return(
-           <View>
-                <TouchableOpacity style={styles.salir} onPress={ ()=> this.logOut()}>
+        <View style={styles.container}>
+            <Image source={require('../../assets/img/background.jpeg')} style={styles.backgroundImage}/>
+            <View style={styles.content}>
+                <Text style={styles.profile}>Mi perfil</Text>
+                <TouchableOpacity style={styles.boton} onPress={ ()=> this.logOut()}>
                     <Text style={styles.textoCentro}>Log out</Text>
                 </TouchableOpacity>
            </View>
+        </View>
        )
    }
 
@@ -40,17 +44,35 @@ const styles = StyleSheet.create({
       flex: 1,
       padding: 20
     },
-    home:{
+    backgroundImage:{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    content:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20
+    },
+    profile:{
         fontWeight:'bold',
         lineHeight:40,
-        fontSize:20
+        fontSize:24,
+        marginBottom: 25,
+        color:'#481E14'
     },
-    salir:{
-        backgroundColor:'rgb(255,165,0)',
-        padding:8,
-        marginBottom:10,
-        marginTop:10,
-        borderRadius:10
+    boton:{
+        backgroundColor:'#A67B5B',
+        width:'50%',
+        borderRadius:10,
+        padding:4,
+        marginTop:20
     },
     textoCentro:{
         textAlign:'center'
