b0bf04048643e75c493550244dd2f5f564afb0d2
mbeorlegui@udesa.edu.ar <mbeorlegui@udesa.edu.ar>
2024-11-18 22:18:11 -0300

    hice cambios de estilo en Post

--- proyectoRn/src/components/Post.js
+++ proyectoRn/src/components/Post.js
@@ -32,25 +32,25 @@ class Post extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Possteado por: {this.props.postInfo.data.owner}
+        <Text style={styles.textoPrincipal}>
+          Publicado por: {this.props.postInfo.data.owner}
         </Text>
-        <Text>
-          Descripcion: {this.props.postInfo.data.mensaje}  
+        <Text style={styles.textoSecundario}>
+          Descripción: {this.props.postInfo.data.mensaje}  
         </Text>
-        <Text>
+        <Text style={styles.textoSecundario}>
           {new Date(this.props.postInfo.data.createdAt).toLocaleString()}  
         </Text>
         { this.state.liked ? (
-          <TouchableOpacity onPress={() => this.handleLike()}> 
-            <Text>Dislike </Text> 
+          <TouchableOpacity style={styles.boton} onPress={() => this.handleLike()}> 
+            <Text style={styles.textoCentro} >Dislike </Text> 
           </TouchableOpacity>
         ) : (
-          <TouchableOpacity onPress={() => this.handleDisLike()}> 
-            <Text>like</Text> 
+          <TouchableOpacity style={styles.boton} onPress={() => this.handleDisLike()}> 
+            <Text style={styles.textoCentro}>Like</Text> 
           </TouchableOpacity>
         )}
-        <Text> Cantidad de likes:{ }</Text>
+        <Text style={styles.textoSecundario}> Cantidad de likes:{ }</Text>
 
       </View>
     );
@@ -58,68 +58,40 @@ class Post extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width:'100%',
-        flex: 1,
-        padding: 20
-      },
-      backgroundImage:{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          width: '100%',
-          height: '100%',
-          resizeMode: 'cover',
-      },
-      content:{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingHorizontal: 20
-      },
-      nuevoPosteo:{
-          fontWeight:'bold',
-          lineHeight:40,
-          fontSize:24,
-          marginBottom: 25,
-          color:'#481E14'
-      },
-      boton:{
-          backgroundColor:'#A67B5B',
-          width:'50%',
-          borderRadius:10,
-          padding:4,
-          marginTop:20
-      },
-      textoCentro:{
-          textAlign:'center'
-      },
-      field:{
-          width: '100%',
-          height: 35,
-          borderWidth: 1,
-          borderColor: '#ccc',
-          borderRadius: 10,
-          marginBottom: 15,
-          paddingLeft: 15,
-          backgroundColor: '#FBFBFB',
-          fontSize: 14,
-          shadowColor:"#000",
-          shadowOffset:{
-              width:0,
-              height:2
-          },
-          shadowOpacity:0.25,
-          shadowRadius: 3.84,
-          elevation: 5,
-      },
-      error: {
-          color: "red",
-          marginBottom: 10,
-          textAlign: "center",
-      }
+    container:{
+        backgroundColor: "#fff",
+        padding: 15,
+        marginVertical: 8,
+        shadowColor:"#000",
+        shadowOffset:{
+            width:0,
+            height:2
+        },
+        shadowOpacity:0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    textoPrincipal: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333',
+        marginBottom:10
+    },
+    textoSecundario: {
+        fontSize: 14,
+        color: '#666',
+    },
+    boton:{
+        backgroundColor:'#A67B5B',
+        width:'20%',
+        borderRadius:10,
+        padding:4,
+        marginTop:20,
+        marginBottom:15
+    },
+    textoCentro:{
+        textAlign:'center'
+    },
 });
 
 export default Post;
